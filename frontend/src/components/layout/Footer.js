94b4bf1094b19849b00ce5dda5df38afe9df59fe
michaeling10 <michaeling10@hotmail.com>
2022-10-23 14:45:24 -0500

    Draft # 2 Front

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+
+const Footer = () => {
+  return (
+    <Fragment>
+
+        <footer className='py-1'>
+            <p className='text-center mt-1'>
+                Pied Piper Clothes Store - 2022 - Mision TIC - Todos los derechos reservados
+            </p>
+        </footer>
+
+    </Fragment>
+  )
+}
+
+export default Footer
