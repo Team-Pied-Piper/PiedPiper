ca5d412dbd4491b0cd728d090cb78f44b9e94191
michaeling10 <michaeling10@hotmail.com>
2022-11-09 21:12:30 -0500

    Orders functionality Back

--- backend/app.js
+++ backend/app.js
@@ -16,9 +16,11 @@ app.use(cookieParser());
 //Importar rutas
 const productos = require("./routes/products")
 const usuarios = require("./routes/auth")
+const ordenes = require("./routes/orders")
 
 app.use('/api',productos) //Pagina principal
 app.use('/api',usuarios)
+app.use('/api',ordenes)
 
 
 //MiddleWares para manejar errores
