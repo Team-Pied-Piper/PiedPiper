55ae3696d27f2a9a3a05b1ab858017c024bf3845
michaeling10 <michaeling10@hotmail.com>
2022-11-17 21:26:40 -0500

    Working Good :+1:  17/11/2022

--- frontend/src/components/layout/MetaData.js
+++ frontend/src/components/layout/MetaData.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react'
 import {Helmet} from 'react-helmet'
 
 const MetaData = ({title}) => {
     return(
         <Helmet>
-            <title>{`Pied Piper - ${title}`}</title>
+            <title>{`${title} - Pied Piper`}</title>
         </Helmet>
     )
-}
 
+}
 export default MetaData
